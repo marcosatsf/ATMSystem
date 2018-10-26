4985a8cc4a329efb0bf12fda0c4d1103dbc6b4a5
marcosatsf <marcos_atsf@hotmail.com>
2018-10-26 20:04:43 -0300

    Layered Frames

--- MenuFrame.java
+++ MenuFrame.java
@@ -1,8 +1,11 @@
 package programa;
 
 import javax.swing.UIManager;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
@@ -14,19 +17,22 @@ import javax.swing.*;
 
 public class MenuFrame extends JFrame{
 	
-	public Font bankFont;
-	public GridLayout gridLayout = new GridLayout(1,4);
+	public static MenuFrame instancia=null;
+	public Font bankFont = new Font("Trebuchet MS", Font.BOLD, 14);
+	public GridLayout gridLayout = new GridLayout(4,1,30,10);
 	public Dimension dimensao = Toolkit.getDefaultToolkit().getScreenSize();
 	
-	public MenuFrame(){
+	private MenuFrame(){
 		super("Banco MBank");
-		super.setLayout(gridLayout);
+		super.setLayout(new GridLayout(2,1));
+		getContentPane().setBackground(Color.ORANGE);
+		//JPanel painelImagem = new JPanel();
+		//painelImagem.setLayout();
+		
+		//painel.setBackground(Color.ORANGE);
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		super.setSize(500,500);
-		super.setVisible(true);
-		super.setLocationRelativeTo(null);
 		super.setResizable(false);
-		
 		this.setLocation((dimensao.width - this.getSize().width)/2, (dimensao.height - this.getSize().height)/2);
 		
 		UIManager.LookAndFeelInfo[] styles = UIManager.getInstalledLookAndFeels();
@@ -38,49 +44,58 @@ public class MenuFrame extends JFrame{
 		{
 			NotStyle.printStackTrace();
 		}
-		
-		getContentPane().setLayout(null);
-		
+				
+		JPanel painelBanner = new JPanel(new FlowLayout());
+		painelBanner.setBackground(Color.ORANGE);
+		//painelBanner.setAlignmentY(BOTTOM_ALIGNMENT);
+		/*
+		 * Inicio da inserção do GIF
+		 */		
 		URL url = this.getClass().getResource("mbank.gif");
 		Icon myImgIcon = new ImageIcon(url);
-		JLabel imageLbl = new JLabel(myImgIcon);
-		imageLbl.setBounds(50, 30, 400, 100);
-		add(imageLbl);
-		
+		JLabel imageLabel = new JLabel(myImgIcon);
+		imageLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		//imageLabel.setBounds(50, 30, 400, 100);
+		//Fim
 		JLabel frontText = new JLabel("Bem-Vindo ao seu banco!");
-		JLabel auxText = new JLabel("Escolha uma das opções abaixo: ");
-				
-		bankFont = new Font("Trebuchet MS", Font.BOLD, 10);
-		
-		frontText.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
-		auxText.setFont(bankFont);
-		frontText.setBounds(100,150,300,50);
+		frontText.setFont(new Font("Trebuchet MS", Font.BOLD, 27));
 		frontText.setHorizontalAlignment(SwingConstants.CENTER);
+		frontText.setVerticalAlignment(SwingConstants.BOTTOM);
 		
-		auxText.setBounds(100,200,300,50);
+		JPanel painel = new JPanel();
+		painel.setLayout(gridLayout);
+		painel.setBackground(Color.ORANGE);
+		painel.setPreferredSize(new Dimension(100,50));
+		//painel.setBounds(50, 150, 400, 300);
+		
+		JPanel painelButton = new JPanel(new FlowLayout());
+		painelButton.setBackground(Color.ORANGE);
+		
+		JLabel auxText = new JLabel("Escolha uma das opções abaixo: ");
+		auxText.setFont(bankFont);
 		auxText.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		add(frontText);
-		add(auxText);
-		
 		JButton gerente = new JButton("Gerente");
+		gerente.setPreferredSize(new Dimension(100,40));
+		
 		JButton cliente = new JButton("Cliente");
+		cliente.setPreferredSize(new Dimension(100,40));
 		
-		gerente.setBounds(200,250,100,50);
-		cliente.setBounds(200,330,100,50);
+		painelBanner.add(imageLabel);
+		painelBanner.add(frontText);
+		add(painelBanner);
 		
-		add(gerente);
-		add(cliente);
-		
-		getContentPane().setBackground(Color.ORANGE);
-
+		painel.add(auxText);
+		painelButton.add(gerente);
+		painelButton.add(cliente);
+		painel.add(painelButton);
+		add(painel);
 		
 		gerente.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
 				// TODO Auto-generated method stub
-				GerenteJFrame subGerente = new GerenteJFrame();
-				subGerente.setLocationRelativeTo(frontText);
+				GerenteJFrame subGerente = new GerenteJFrame(dimensao, MenuFrame.this);
 				setVisible(false);
 				//dispose();
 			}
@@ -90,15 +105,11 @@ public class MenuFrame extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent event) {
 				// TODO Auto-generated method stub
-				ClienteJFrame subCliente = new ClienteJFrame();
-				subCliente.setSize(500,500);
-				subCliente.setVisible(true);
-				subCliente.setLocationRelativeTo(frontText);
+				ClienteJFrame subCliente = new ClienteJFrame(dimensao, MenuFrame.this);
 				setVisible(false);
 				//dispose();
 			}
 		});
-		//pack();
 		addWindowListener(new java.awt.event.WindowAdapter(){
 			@Override
 			public void windowClosing(java.awt.event.WindowEvent event)
@@ -107,6 +118,17 @@ public class MenuFrame extends JFrame{
 				//X on close
 			}
 		});
+		
+		super.setVisible(true);
+	}
+	
+	public static MenuFrame getMenuFrame()
+	{
+		if(instancia==null)
+		{
+			return new MenuFrame();
+		}
+		else return instancia;
 	}
 
 }
